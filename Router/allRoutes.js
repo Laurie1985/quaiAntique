73ec7e20a7887ebb1ba440c2d1d5ae5b6089bc93
Laurie1985 <berthon.laurie@neuf.fr>
2025-09-24 21:12:30 +0200

    gestion de l'affichage des réservations

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,14 +3,13 @@ import Route from "./Route.js";
 //Définition des routes de l'application
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/galerie", "La galerie", "/pages/galerie.html"),
-    new Route("/menu", "Les menus", "/pages/menu.html"),
+    new Route("/galerie", "Galerie", "/pages/galerie.html"),
+    new Route("/menu", "La carte", "/pages/menu.html"),
     new Route("/signin", "connexion", "/pages/auth/signin.html"),
     new Route("/signup", "inscription", "/pages/auth/signup.html"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
-    new Route("/editPassword", "Modifier mon mot de passe", "/pages/auth/editPassword.html"),
-    new Route("/contact", "Contact", "/pages/contact.html"),
-    new Route("/reservation", "Réservation", "/pages/reservation.html"),
+    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
+    new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
